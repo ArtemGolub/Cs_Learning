9fd5d1928e28cd4f995631c605f47484fc187e15
Artem Golub <otodraliti@gmail.com>
2025-02-04 18:31:10 +0300

    feat: string operations

--- ConsoleLearning/ConsoleLearning/Program.cs
+++ ConsoleLearning/ConsoleLearning/Program.cs
@@ -1,14 +1,31 @@
-﻿
-char c = 'A';
-char newLine = '\n' ;
-char backSlash = '\\' ;
+﻿string a = "Heat";
+string b = "Heat";
+string c = "Here's a tab:\t";
+Console.WriteLine(c + (a==b));
 
-char charNumber = '1';
+string a1 = @"\\server\fileshare\file.txt";
+string xml = @"<customer id= ""12345""></customer>";
+Console.WriteLine(xml);
 
-ushort ushortNumber = charNumber;
-int intNumber = charNumber;
-uint uintNumber = charNumber;
-long longNumber = charNumber;
-ulong ulongNumber = charNumber;
+string concat = "a" + "b";
+Console.WriteLine(concat);
 
-sbyte floatNumber = (sbyte)charNumber;
+
+string concatNum = "a" + 5;
+Console.WriteLine(concatNum);
+
+int x = 1;
+string interpolatedString = $"The value of x is {x}";
+Console.WriteLine(interpolatedString);
+
+string s = $"255 in hex is {byte.MaxValue:X2}";
+Console.WriteLine(s);
+
+bool z = true;
+Console.WriteLine($"The answer in binary is {(z ? 1: 0)}");
+
+const string greeting = "Hello!";
+const string message = $"{greeting}, world";
+
+ReadOnlySpan<byte> utf8 = "ab→св"u8;
+Console.WriteLine(utf8.Length);
