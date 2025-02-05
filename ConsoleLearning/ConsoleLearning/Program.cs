b7583fbf96d6bd9ed50a50992bc44b36b3855717
Artem Golub <otodraliti@gmail.com>
2025-02-05 12:05:40 +0300

    feat: arrays

--- ConsoleLearning/ConsoleLearning/Program.cs
+++ ConsoleLearning/ConsoleLearning/Program.cs
@@ -1,21 +1,17 @@
-﻿char[] vowels = ['a', 'b', 'c', 'd', 'e'];
+﻿var vowels = new[] { 'a', 'b', 'c', 'd', 'e' };
 
-Range firstTwoRange = 0..2;
-char[] firstTwo = vowels[firstTwoRange];
-
-Console.WriteLine(firstTwo);
-
-int[][] jaggedArray = new int[3][];
-jaggedArray[0] = new int[] { 1, 2 };       // Первая строка с 2 элементами
-jaggedArray[1] = new int[] { 3, 4, 5 };    // Вторая строка с 3 элементами
-jaggedArray[2] = new int[] { 6, 7, 8, 9 }; // Третья строка с 4 элементами
-
-// Вывод зубчатого массива
-for (int i = 0; i < jaggedArray.Length; i++) // Обход строк
+var rectangulraMatrix = new[,]
 {
-    for (int j = 0; j < jaggedArray[i].Length; j++) // Обход элементов в строке
-    {
-        Console.Write(jaggedArray[i][j] + " ");
-    }
-    Console.WriteLine(); // Переход на новую строку после завершения текущей строки
-}
+    { 0, 1, 2 },
+    { 3, 4, 5 },
+    { 6, 7, 8 }
+};
+
+var jaggedMatrix = new int[][]
+{
+    new int[] { 0, 1, 2 },
+    new int[] { 3, 4, 5 },
+    new int[] { 6, 7, 8, 9 }
+};
+
+
