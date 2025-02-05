2f6fddba567dbfa7635a29d8844209f9bc056015
Artem Golub <otodraliti@gmail.com>
2025-02-05 11:49:59 +0300

    feat: ragged array

--- ConsoleLearning/ConsoleLearning/Program.cs
+++ ConsoleLearning/ConsoleLearning/Program.cs
@@ -1,10 +1,21 @@
 ﻿char[] vowels = ['a', 'b', 'c', 'd', 'e'];
 
-char[] firstTwo = vowels[..2];
-char[] lastThree = vowels[2..];
-char[] middleOne = vowels[2..3];
+Range firstTwoRange = 0..2;
+char[] firstTwo = vowels[firstTwoRange];
 
 Console.WriteLine(firstTwo);
-Console.WriteLine(lastThree);
-Console.WriteLine(middleOne);
 
+int[][] jaggedArray = new int[3][];
+jaggedArray[0] = new int[] { 1, 2 };       // Первая строка с 2 элементами
+jaggedArray[1] = new int[] { 3, 4, 5 };    // Вторая строка с 3 элементами
+jaggedArray[2] = new int[] { 6, 7, 8, 9 }; // Третья строка с 4 элементами
+
+// Вывод зубчатого массива
+for (int i = 0; i < jaggedArray.Length; i++) // Обход строк
+{
+    for (int j = 0; j < jaggedArray[i].Length; j++) // Обход элементов в строке
+    {
+        Console.Write(jaggedArray[i][j] + " ");
+    }
+    Console.WriteLine(); // Переход на новую строку после завершения текущей строки
+}
