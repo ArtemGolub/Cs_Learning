1387bd183ea610eb179c69a89a61f6cf166e429d
Artem Golub <otodraliti@gmail.com>
2025-02-06 12:24:48 +0300

    feat: do-while, whil, for, switch, goto

--- ConsoleLearning/ConsoleLearning/Program.cs
+++ ConsoleLearning/ConsoleLearning/Program.cs
@@ -1,7 +1,32 @@
-﻿for (int i = 0, prevFib = 1, cureFib = 1; i < 10; i++)
+﻿int x = 0;
+while (true)
 {
-    Console.WriteLine(prevFib);
-    int newFib = prevFib + cureFib;
-    prevFib = cureFib;
-    cureFib = newFib;
+    if(x++ > 5)
+        break;
 }
+
+for (int i = 0; i < 10; i++)
+{
+    if((i % 2) == 0)
+        continue;
+    
+    Console.WriteLine(i);
+}
+
+int y = default;
+switch (y)
+{
+    case 0:
+    {
+        goto case 1;
+    }
+    case 1:
+    {
+        Console.WriteLine(y);
+        break;
+    }
+}
+
+string w = null;
+if (w == null)
+    throw new ArgumentException("My String for this exception");
