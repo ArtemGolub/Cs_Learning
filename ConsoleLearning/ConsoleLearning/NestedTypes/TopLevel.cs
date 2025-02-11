3a6cc0b5bdbb5d00ba3999cf9adbc196b817075f
Artem Golub <otodraliti@gmail.com>
2025-02-11 18:03:45 +0300

    feat: nested types

---
+++ ConsoleLearning/ConsoleLearning/NestedTypes/TopLevel.cs
@@ -0,0 +1,12 @@
+namespace ConsoleLearning.NestedTypes;
+
+public class TopLevel
+{
+    public class Nested{ } // Вложенный класс
+    public enum Color // Вложенное перечисление
+    {
+        Red,
+        Blue,
+        Tan
+    }
+}
