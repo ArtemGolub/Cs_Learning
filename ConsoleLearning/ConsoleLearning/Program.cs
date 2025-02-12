3c520cec1c8b8b2b4e46323a4b0af6192b83760a
Artem Golub <otodraliti@gmail.com>
2025-02-12 14:18:42 +0300

    feat: Extension Methods with Delegates

--- ConsoleLearning/ConsoleLearning/Program.cs
+++ ConsoleLearning/ConsoleLearning/Program.cs
@@ -1,6 +1,14 @@
 ﻿using ConsoleLearning.SpecialTypes;
 
 MathOperations mathOperations = new MathOperations();
-Transformer t = mathOperations.Square;
-int answer = t(5);
-Console.WriteLine(answer);
+int[] values = {1,2,3};
+
+Transform (values, mathOperations.Square);
+
+foreach (int value in values)
+    Console.WriteLine(value + " ");
+void Transform(int[] values, Transformer t)
+{
+    for (int i = 0; i < values.Length; i++)
+        values[i] = t(values[i]);
+}
