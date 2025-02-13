585cc4b87ea375b0e0680b90694329a024c730fe
Artem Golub <otodraliti@gmail.com>
2025-02-13 16:43:46 +0300

    feat: lambda expression return types

--- ConsoleLearning/ConsoleLearning/SpecialTypes/LambdaExpressions.cs
+++ ConsoleLearning/ConsoleLearning/SpecialTypes/LambdaExpressions.cs
@@ -8,4 +8,14 @@ public class LambdaExpressions
 
     private Func<string, string, int> totalLength = (s1, s2) => s1.Length + s2.Length;
     private int total => totalLength("hello", "World");
+
+
+    void Foo<T>(T x) { }
+    void Bar<T>(Action<T> a) { }
+
+    public void MyMethod()
+    {
+        Func<int, int> sqr = int (int x) => x;
+        var sqr2 = int (int x) => x;
+    }
 }
