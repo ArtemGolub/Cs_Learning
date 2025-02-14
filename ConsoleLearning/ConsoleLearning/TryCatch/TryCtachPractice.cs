dfbbba7a7c6d1ca62d1950ab6f366d60899cd216
Artem Golub <otodraliti@gmail.com>
2025-02-14 15:35:42 +0300

    feat: ArgumentNullException ThrowIfNull

--- ConsoleLearning/ConsoleLearning/TryCatch/TryCtachPractice.cs
+++ ConsoleLearning/ConsoleLearning/TryCatch/TryCtachPractice.cs
@@ -4,52 +4,15 @@ namespace ConsoleLearning.TryCatch;
 
 public class TryCtachPractice
 {
-    public void MyMethod(string[] args)
+    public void MyMethod()
     {
-        try
-        {
-            byte b = byte.Parse(args[0]);
-            Console.WriteLine();
-        }
-        catch (FormatException ex)
-        {
-            Console.WriteLine($"{ex} : Please provide at least one argument");
-        }
-        catch (IndexOutOfRangeException) // можно опустить переменную
-        {
-            Console.WriteLine("Please provide at least one argument");
-        }
-        catch (WebException ex) when (ex.Status == WebExceptionStatus.Timeout) // Можно создавать фильтры исключений
-        {
-            
-        }
-        catch // Опуская переменную перехватываются все исключения
-        {
-        } 
+        Display(null);
     }
-
-    void ReadFile()
+    
+    void Display(string name)
     {
-        StreamReader reader = null;
-        try
-        {
-            reader = File.OpenText("file.txt");
-            if (reader.EndOfStream) return;
-            Console.WriteLine(reader.ReadToEnd());
-        }
-        finally
-        {
-            if(reader != null) reader.Dispose();
-        }
-    }
-
-    void ReadFileUsing()
-    {
-        if (File.Exists("file.txt"))
-        {
-            using var reader = File.OpenText("file.txt");
-            Console.WriteLine(reader.ReadToEnd());
-        }
+        ArgumentNullException.ThrowIfNull(name);
+        Console.WriteLine(name);
     }
 
 }
