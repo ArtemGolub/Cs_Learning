93c25cca5449c4e631abcfaff5b6020f8bc97fcc
Artem Golub <otodraliti@gmail.com>
2025-02-18 08:50:00 +0300

    feat: Atribute: CallerArgumentExpression

--- ConsoleLearning/ConsoleLearning/Program.cs
+++ ConsoleLearning/ConsoleLearning/Program.cs
@@ -1,18 +1,18 @@
-﻿
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 public class Program
 {
-    static void Main() => Foo();
-
-    static void Foo(
-        [CallerMemberName] string? memberName = null,
-        [CallerFilePath] string? filePath = null,
-        [CallerLineNumber] int lineNumber = 0
-        )
+    static void Main()
     {
-        Console.WriteLine($"{memberName}");
-        Console.WriteLine($"{filePath}");
-        Console.WriteLine($"{lineNumber}");
+        int number = 5;
+        LogExpression(number > 10);
+        LogExpression(number == 5);
+    }
+
+    static void LogExpression(
+        bool condition,
+        [CallerArgumentExpression("condition")] string? expression = null)
+    {
+        Console.WriteLine($"Выражение: {expression}, результат: {condition}");
     }
 }
