401794f28da87a974976792f8486904072173201
Artem Golub <otodraliti@gmail.com>
2025-02-18 10:28:17 +0300

    feat: static polymorf

--- ConsoleLearning/ConsoleLearning/Program.cs
+++ ConsoleLearning/ConsoleLearning/Program.cs
@@ -1,18 +1,34 @@
-﻿using System.Runtime.CompilerServices;
+﻿interface ICreateRandom<T>
+{
+    static abstract T CreateRandom();
+}
+
+record Point(int X, int Y) : ICreateRandom<Point>
+{
+     static Random _rnd = new ();
+     public static Point CreateRandom() => new ( _rnd.Next(), _rnd.Next());
+     
+
+
+}
 
 public class Program
 {
+
+    static T[] CreateTestData<T> (int count) where T: ICreateRandom<T>
+    {
+        T[] data = new T[count];
+        for (int i = 0; i < count; i++)
+            data[i] = T.CreateRandom();
+    
+        return data;
+    }
     static void Main()
     {
-        int number = 5;
-        LogExpression(number > 10);
-        LogExpression(number == 5);
-    }
-
-    static void LogExpression(
-        bool condition,
-        [CallerArgumentExpression("condition")] string? expression = null)
-    {
-        Console.WriteLine($"Выражение: {expression}, результат: {condition}");
+        Point[] testData = CreateTestData<Point>(50);
+        foreach (var item in testData)
+        {
+            Console.WriteLine(item.X + " " + item.Y);
+        }
     }
 }
--- ConsoleLearning/ConsoleLearning/Variables/Point.cs
+++ ConsoleLearning/ConsoleLearning/Variables/Point.cs
@@ -1,9 +1,9 @@
 namespace ConsoleLearning.Variables;
 
-ref struct Point
+public struct Point(int x, int y)
 {
-    public readonly int X, Y;
-
-    public Point(int x, int y) => (X, Y) = (x, y);
-
+    public readonly int X = x, Y = y;
+    
+    public static Point operator +(Point a, Point b) => new(a.X + b.X, a.Y + b.Y);
+    public override string ToString() => $"({X}, {Y})";
 }
