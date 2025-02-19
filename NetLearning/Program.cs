5092997f6a6d8e99f984b4a12ca9582a51ce22b0
Artem Golub <otodraliti@gmail.com>
2025-02-19 12:36:01 +0300

    feat: simple NumberFormatInfo

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -1,11 +1,7 @@
-﻿using NetLearning.Formatting_and_Parsing;
+﻿
 
-ToString_Parse<int> parser = new ToString_Parse<int>();
+using System.Globalization;
 
-int parsingString = 1;
-string? s = parser.Convert(parsingString);
-parsingString = 0;
-Console.WriteLine(parsingString);
-parser.DeConvert(s, out int data);
-parsingString = data;
-Console.WriteLine(parsingString);
+NumberFormatInfo f = new NumberFormatInfo();
+f.CurrencySymbol = "$$";
+Console.WriteLine(3.ToString("C", f));
