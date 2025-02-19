9d21c49abc8f7a2569b5566ae93b02ac602a5444
Artem Golub <otodraliti@gmail.com>
2025-02-19 16:39:29 +0300

    feat: int to object Enum

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -1,18 +1,3 @@
 ﻿
-int i = (int) BorderSides.Top;
-BorderSides side = (BorderSides) i;
-Console.WriteLine(GetIntegralValueAsString(side));
-
-
-static int GetIntegralValue(Enum anyEnum) => (int)(object)anyEnum;
-
-static object GetBoxedIntegralValue(Enum anyEnum)
-{
-    Type integralType = Enum.GetUnderlyingType(anyEnum.GetType());
-    Console.WriteLine(integralType);
-    return Convert.ChangeType(anyEnum, integralType);
-}
-
-static decimal GetAnyIntegerValue(Enum anyEnum) => Convert.ToDecimal(anyEnum);
-
-static string GetIntegralValueAsString (Enum anyEnum) => anyEnum.ToString("D");
+object bs = Enum.ToObject(typeof(BorderSides), 7);
+Console.WriteLine(bs);
