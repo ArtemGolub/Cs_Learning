226a64b1b59c1d8632090a3e15221c7585209421
Artem Golub <otodraliti@gmail.com>
2025-02-19 16:05:18 +0300

    feat: random with seeds

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -1,6 +1,8 @@
-﻿using System.Numerics;
+﻿
 
-BigInteger googol = BigInteger.Pow(10, 100);
-double g2 = (double) googol;
-BigInteger g3 = (BigInteger) g2;
-Console.WriteLine(g3);
+Random r1 = new Random(1);
+Random r2 = new Random(1);
+
+
+Console.WriteLine($"{r1.Next(100)} , {r1.Next(100)}");
+Console.WriteLine($"{r2.Next(100)} , {r2.Next(100)}");
