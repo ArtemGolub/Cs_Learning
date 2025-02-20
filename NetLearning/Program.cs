97b3ea7ca3d3b26f999f9f244d3193613f56124b
Artem Golub <otodraliti@gmail.com>
2025-02-20 09:49:24 +0300

    feat: virtual and static Equals

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -1,2 +1,9 @@
-﻿Guid g = Guid.Empty;
-Console.WriteLine(g.ToString());
+﻿int x = 5;
+int y = 5;
+Console.WriteLine(x == y);
+
+object a = 5;
+object b = 5;
+Console.WriteLine(a.Equals(b));
+a = null;
+Console.WriteLine(Equals(a, b));
