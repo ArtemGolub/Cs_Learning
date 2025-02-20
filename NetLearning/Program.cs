c4da4e3ebc7217ca1b32986f68ec5663502fd2e4
Artem Golub <otodraliti@gmail.com>
2025-02-20 09:37:04 +0300

    feat: guid example

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -1,9 +1,2 @@
-﻿
-foreach (Enum value in Enum.GetValues(typeof(BorderSides)))
-{
-    Console.WriteLine(value);
-}
-
-Array values = Enum.GetValues(typeof(BorderSides));
-object? lastElement = values.GetValue(values.Length - 1);
-Console.WriteLine(lastElement);
+﻿Guid g = Guid.Empty;
+Console.WriteLine(g.ToString());
