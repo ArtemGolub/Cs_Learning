7ac0d5d6d414c29ea6a8234c610fc29318b778f3
Artem Golub <otodraliti@gmail.com>
2025-02-21 10:31:38 +0300

    feat: converting arrays

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -1,11 +1,2 @@
-﻿int[] numbers = {1,2,3,4,5};
-Array.Sort(numbers, (x,y) 
-    => x % 2 == y % 2 
-        ? 0 
-        : x % 2 == 1
-            ? -1
-            : 1
-       );
-
-foreach (int number in numbers)
-    Console.Write(number);
+﻿float[] reals = {1.3f, 1.5f, 1.6f};
+int[] whiles = Array.ConvertAll(reals, r => Convert.ToInt32(r));
