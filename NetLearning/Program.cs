74f492d6b48e5cde11df1ba983d712ff8b5c061a
Artem Golub <otodraliti@gmail.com>
2025-02-21 11:20:17 +0300

    feat: stack example

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -1,9 +1,11 @@
-﻿Queue<int> q = new Queue<int>();
-q.Enqueue(10);
-q.Enqueue(20);
+﻿Stack<int> stack = new Stack<int>();
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
 
-int[] data = q.ToArray();
-Console.WriteLine(q.Count);
-Console.WriteLine(q.Peek());
-Console.WriteLine(q.Dequeue());
-Console.WriteLine(q.Dequeue());
+int[] data = stack.ToArray();
+Console.WriteLine(stack.Count);
+Console.WriteLine(stack.Peek());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
