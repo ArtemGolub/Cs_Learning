352d083fd54a8bbd126d7b255e6589207bf4253a
Artem Golub <otodraliti@gmail.com>
2025-02-21 12:58:58 +0300

    refactoring: updated OrderedDictionary

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Specialized;
 
-OrderedDictionary oD = new OrderedDictionary();
-oD.Add(3, "Three");
-oD.Add(1, "One");
-oD.Add(2, "Two");
+OrderedDictionary oD = new OrderedDictionary
+{
+    { 3, "Three" },
+    { 1, "One" },
+    { 2, "Two" }
+};
 
 for (int i = 0; i < oD.Count; i++)
-    Console.WriteLine(oD[i].ToString());
+    Console.WriteLine(oD[i]?.ToString());
 
 foreach (DictionaryEntry entry in oD)
     Console.WriteLine($"Key: {entry.Key}, Value: {entry.Value}");
