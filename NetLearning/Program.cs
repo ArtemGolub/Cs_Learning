8064446b64d034383cd95c4053c48c9199586e20
Artem Golub <otodraliti@gmail.com>
2025-02-21 12:44:51 +0300

    feat: OrderedDictionary example

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -1,7 +1,13 @@
-﻿
-Dictionary<string, int> d = new Dictionary<string, int>();
+﻿using System.Collections;
+using System.Collections.Specialized;
 
-foreach (KeyValuePair<string, int> kvp in d)
-{
-    
-}
+OrderedDictionary oD = new OrderedDictionary();
+oD.Add(3, "Three");
+oD.Add(1, "One");
+oD.Add(2, "Two");
+
+for (int i = 0; i < oD.Count; i++)
+    Console.WriteLine(oD[i].ToString());
+
+foreach (DictionaryEntry entry in oD)
+    Console.WriteLine($"Key: {entry.Key}, Value: {entry.Value}");
