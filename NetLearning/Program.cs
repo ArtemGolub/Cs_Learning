93154d6932c3a18bddb54b8a86b8a865b85d794d
Artem Golub <otodraliti@gmail.com>
2025-02-21 09:34:38 +0300

    feat: array searching

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -1,11 +1,6 @@
 ﻿
-int[] myArray = {1,2,3};
-int first = myArray[0];
-int last = myArray[^1];
+string[] names = { "Rodney", "Jack", "Jill" };
+string match = Array.Find(names, n => n.Contains("a"));
+Console.WriteLine($"Match: {match}");
 
-Console.WriteLine($"{first} {last}");
 
-Array a = Array.CreateInstance(typeof(string), 2);
-a.SetValue("hi", 0);
-a.SetValue("there", 1);
-string? s = (string) a.GetValue(0)!;
