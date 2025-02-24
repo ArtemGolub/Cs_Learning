5da19f20dab13283a89eb118ef38c881b3602c09
Artem Golub <otodraliti@gmail.com>
2025-02-24 16:27:02 +0300

    feat: simple Linq

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -1,21 +1,15 @@
-using NetLearning.EqualityComparers;
-
 namespace NetLearning;
 
 public class Program
 {
     public static void Main()
     {
-        var wishList = new List<Wish>();
-        wishList.Add(new Wish("Peace", 2));
-        wishList.Add(new Wish("Wealth", 3));
-        wishList.Add(new Wish("Love", 2));
-        wishList.Add(new Wish("3 more wishes", 1));
-        
-        wishList.Sort(new PriorityComparer());
+        string[] names = { "Tom", "Dick", "Harry" };
+        IEnumerable<string> filtedNames = names
+            .Where(n => n.Length > 3);
 
-        foreach (Wish wish in wishList)
-            Console.Write($"{wish.Name} |");
+        foreach (string name in filtedNames)
+            Console.WriteLine(name);
     }
     
 }
