bea13682fe36c3374ce8586552885b63b0edc802
Artem Golub <otodraliti@gmail.com>
2025-02-24 13:20:05 +0300

    feat: EqualityComparer<T>.Default

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -6,13 +6,12 @@ public class Program
 {
     public static void Main()
     {
-        Customer c1 = new("Bloggs", "Joe");
-        Customer c2 = new("Bloggs", "Joe");
-        
-        LastFirstEqComparer eqComparer = new();
-        
-        Dictionary<Customer, string> customers = new(eqComparer);
-        customers[c1] = "Joe";
-        Console.Write(customers.ContainsKey(c2)); // true
+
+    }
+
+    static bool Foo<T>(T x, T y)
+    {
+        bool same = EqualityComparer<T>.Default.Equals(x, y);
+        return same;
     }
 }
