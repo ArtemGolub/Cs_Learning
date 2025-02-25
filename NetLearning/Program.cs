66071e8ac204f6ec205e5bbd0e1c2048c56fc4e6
Artem Golub <otodraliti@gmail.com>
2025-02-25 12:46:20 +0300

    feat: new linq test

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -4,9 +4,11 @@ public class Program
 {
     public static void Main()
     {
-        string[] names = { "Tom", "Dick", "Harry" };
+        string[] names = { "Tom", "Dick", "Harry", "Mary", "Jay" };
         IEnumerable<string> filtedNames = names
-            .Where(n => n.Length > 3);
+            .Where(n => n.Contains("a"))
+            .OrderBy(n => n.Length)
+            .Select(n => n.ToUpper());
 
         foreach (string name in filtedNames)
             Console.WriteLine(name);
