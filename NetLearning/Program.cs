1cbb73ad8a030f5bdb4597d13ba06fe5074b92e8
Artem Golub <otodraliti@gmail.com>
2025-02-28 12:06:18 +0300

    feat: subquery LINQ

--- NetLearning/Program.cs
+++ NetLearning/Program.cs
@@ -4,20 +4,15 @@ public class Program
 {
     public static void Main()
     {
-        string[] names = { "Tom", "Dick", "Harry", "Mary", "Jay" };
-        IEnumerable<string> filtedNames = names
-            .Where(n => n.Contains("a"))
-            .OrderBy(n => n.Length)
-            .Select(n => n.ToUpper());
+        string[] musos =
+        {
+            "Dabid Gilmour",
+            "Roger Waters",
+            "Rick Wright",
+            "Nick Mason"
+        };
 
-        IEnumerable<string> filtedNames2 =
-            from n in names
-            where n.Contains("a")
-            orderby n.Length
-            select n.ToUpper();
-
-        foreach (string name in filtedNames)
-            Console.WriteLine(name);
+        IEnumerable<string> query = musos.OrderBy(m => m.Split().Last());
     }
     
 }
