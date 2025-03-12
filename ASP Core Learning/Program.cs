2ef89ec4c9b70de088d9eaa91c63d1f7f60ebcc8
Artem Golub <otodraliti@gmail.com>
2025-03-12 13:57:50 +0300

    feat: navigation property loading

--- ASP Core Learning/Program.cs
+++ ASP Core Learning/Program.cs
@@ -4,14 +4,25 @@ using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 using NutShellContext dbContext = new NutShellContext();
 
-List<Customer> cust = dbContext.Customers
-    .Include(c => c.Purchases)
-    .Where(c => c.Purchases.Count >= 2)
-    .ToList();
+var custInfo = dbContext.Customers
+    .Where(c => c.ID == 15)
+    .Select(c => new
+    {
+        Name = c.Name,
+        Purchases = c.Purchases
+            .Select(p => new
+            {
+                p.Description,
+                p.Price
+            })
+    })
+    .First();
 
-foreach (Customer customer in cust)
+foreach (var DP in custInfo.Purchases)
 {
-    Console.WriteLine($"{customer.Name} with {customer.Purchases.Count} purchases");
+    Console.WriteLine($"{custInfo.Name} bought: {DP.Description} with price: {DP.Price}");
 }
+
+
 
 
