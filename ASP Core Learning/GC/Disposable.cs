00fe31c554133e5b5476f62c39af3f6b7f5c1602
Artem Golub <otodraliti@gmail.com>
2025-03-14 13:51:00 +0300

    feat: anon dispose pattern

---
+++ ASP Core Learning/GC/Disposable.cs
@@ -0,0 +1,16 @@
+﻿namespace ASP_Core_Learning.GC;
+
+public class Disposable: IDisposable
+{
+    public static Disposable Create(Action onDispose)
+        => new Disposable(onDispose);
+    
+    private Action _onDispose;
+    Disposable(Action onDispose) => _onDispose = onDispose;
+
+    public void Dispose()
+    {
+        _onDispose?.Invoke(); // выполнить действие освобожения, если не null
+        _onDispose = null; // обеспечить чтобы его нельзя было выполнить еще раз
+    }
+}
